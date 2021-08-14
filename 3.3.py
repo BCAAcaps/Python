a01c26ad3dde635aa0b6393adb158d454c63b8bc
BCAAcaps <72194737+BCAAcaps@users.noreply.github.com>
2021-08-14 14:04:50 +0300

    Update 3.3.py

--- 3.3.py
+++ 3.3.py
@@ -1,4 +1,9 @@
-def my_func(x, y):
-    return 1 / x ** abs(y)
-    return x ** y
-print(my_func(2, -3))
+def my_func(arg1 , arg2, arg3):
+    if arg1 >= arg3 and arg2 >= arg3:
+        return arg1 + arg2
+    elif arg1 > arg2 and arg1 < arg3:
+        return arg1 + arg3
+    else:
+        return arg2 + arg3
+
+print(f'Результат - {my_func(int(input("Введите первый аргумент ")), int(input("Введите второй аргумент ")), int(input("Введите третий аргумент ")))}') 
